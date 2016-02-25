849cf1a15a7cbd5c8c1e32a62004154e1ad1da4e
nickware <naturalezacomputacional@gmail.com>
2016-02-25 12:09:12 -0500

    Agregando Proyecto: Curvas de Lissajous

---
+++ Waves/src/Lissajous.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Lissajous.cpp
+// Author      : Nickware
+// Version     :
+// Copyright   : GPL
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Curve Lissajous" << endl; // prints Curve Lissajous
+	return 0;
+}
