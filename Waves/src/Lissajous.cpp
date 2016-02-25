c0c50a13ace2a5eaff25bc1a9fb055c1bff7ad66
nickware <naturalezacomputacional@gmail.com>
2016-02-25 13:40:36 -0500

    Modificanfo Proyecto: Curvas de Lissajous

--- Waves/src/Lissajous.cpp
+++ Waves/src/Lissajous.cpp
@@ -10,28 +10,35 @@
 #include <stdio.h>
 #include <math.h>
 
+#define pi 3.141592
+
 using namespace std;
+
+//const double PI = 3.1416;
 
 int main()
 {
-	double x;
+
+	double  x;
 	double y;
 	double A;
 	double B;
-	double wx;
-	double wy;
+	double  fx;
+	double fy;
 	double dt;
+	//double delta;
 	double t;
 
 	dt=0.01;
+	//delta=PI/2;
 
-	cout << "Valor para wx" << endl;
-	cin >> wx;
-	cout << "Valor para wy" << endl;
-	cin >> wy;
-	cout << "Valor para A" << endl;
+	cout << "Valor para frecuencia natural en X" << endl;
+	cin >> fx;
+	cout << "Valor para frecuencia natural en Y" << endl;
+	cin >> fy;
+	cout << "Valor para amplitud en X" << endl;
 	cin >> A;
-	cout << "Valor para B" << endl;
+	cout << "Valor para amplitud en Y" << endl;
 	cin >> B;
 
 	FILE *lissajous;
@@ -39,12 +46,15 @@ int main()
 	lissajous = fopen (nombre, "w");
 
 	for (int i=0; i<360; i=i++){
-		t=i*dt;
+		//t=i*delta;
+		t=i+dt;
+		x=A*sin(fx*t+dt);
+		y=B*cos(fy*t);
 
-		x=A*sin(wx+t);
-		y=B*cos(wy+t);
+		//x=A*sin(fx*t);
+		//y=B*cos(fy*t);
 
-		fprint(lissajous, "%f %f $f\n", x, y, t);
+		fprintf(lissajous, "%f %f %f\n", x, y, t);
 
 	}
 	return 0;
