32734b179aa2efa11e36f39e4b88c56451a3f807
CesarMauricioMendozaOsorio <123024577+CesarMendozaO@users.noreply.github.com>
2023-04-21 16:18:14 -0500

    Update EulerMethod.py

--- exam/CMMendozaO/EulerMethod.py
+++ exam/CMMendozaO/EulerMethod.py
@@ -1,6 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
 y0=float(input('ingrese la posición inicial y0='))
 v0=float(input('ingrese la velocidad inicial v0='))
 h=float(input('ingrese el tiempo inicial t0='))
+w=float(input('Paso ='))
 g=9.8
 
 t=[h]
@@ -11,8 +14,8 @@ ye=[y0]
 i=0
 
 
-while ya[i]>0:
-    h=h+0.01
+while ya[i]>0: 
+    h=h+w
     t.append(h)
     i=i+1
     c=-g*t[i]**2/2+t[i]*v0+y0
@@ -21,9 +24,9 @@ while ya[i]>0:
         ya.append(c)
         d=v0-g*t[i]
         va.append(d)
-        e=ve[i-1]-g*0.01
+        e=ve[i-1]-g*w
         ve.append(e)
-        f=ye[i-1]+0.01*ve[i]
+        f=ye[i-1]+w*ve[i]
         if f<0:
             f=0
             ye.append(f)
@@ -33,9 +36,9 @@ while ya[i]>0:
         ya.append(c)
         d=v0-g*t[i]
         va.append(d)
-        e=ve[i-1]-g*0.01
+        e=ve[i-1]-g*w
         ve.append(e)
-        f=ye[i-1]+0.01*ve[i]
+        f=ye[i-1]+w*ve[i]
         if f<0:
             f=0
             ye.append(f)
@@ -44,17 +47,63 @@ while ya[i]>0:
     
 
 
-with open("salida.txt", "w") as archivo: # Prepara mediante write la creacion de un write de extension txt
-    archivo.write("Tiempo\tya\tva\tve\tye\n") # Este es el encabezado de mi write txt
-    for i in range(len(ya)): # Este bucle depende del numero de valores del vector posicion
-        archivo.write(f"{t[i]}\t{ya[i]}\t") # Esta linea imprime en el write los tiempos (ubicados en el vector tiempos) y las posiciones (ubicadas en el vector posiciones)
-        if i < len(va): # Esta estructura de control va guardando las velocidades obtenidas en el vector velocidades, en el write txt
-            archivo.write(f"{va[i]}\t") # Almacena las velocidade al write
-        else:
-            archivo.write("\t") # cuando no encuentra velocidades, pasa a guardar las aceleraciones
-        if i < len(ve): # Esta estructura de control va guardando las aceleraciones obtenidas en el vector aceleraciones, en el write txt
-            archivo.write(f"{ve[i]}\t") # Guarda todas la aceleraciones calculadas en el write
-        if i < len(ye): # Esta estructura de control va guardando las aceleraciones obtenidas en el vector aceleraciones, en el write txt
-            archivo.write(f"{ye[i]}") # Guarda todas la aceleraciones calculadas en el write
-        archivo.write("\n") # Instruccion que indica fin de la linea
+with open("ya.txt", "w") as archivo: 
+    for i in range(len(ya)): 
+        archivo.write(f"{t[i]}\t,{ya[i]}\t") 
+        archivo.write("\n") 
 
+x, y = np.loadtxt('ya.txt', delimiter=',', unpack=True)
+
+plt.plot(x,y, label='Gráfica')
+
+ 
+plt.xlabel('t')
+plt.ylabel('Posición Analítica ya(t)')
+plt.legend()
+plt.show()
+
+
+with open("va.txt", "w") as archivo: 
+    for i in range(len(va)): 
+        archivo.write(f"{t[i]}\t,{va[i]}\t") 
+        archivo.write("\n") 
+
+z, w = np.loadtxt('va.txt', delimiter=',', unpack=True)
+
+plt.plot(z,w, label='Gráfica')
+
+ 
+plt.xlabel('t')
+plt.ylabel('Velocidad Analítica va(t)')
+plt.legend()
+plt.show()
+
+with open("ve.txt", "w") as archivo: 
+    for i in range(len(ve)): 
+        archivo.write(f"{t[i]}\t,{ve[i]}\t") 
+        archivo.write("\n") 
+
+a, b = np.loadtxt('va.txt', delimiter=',', unpack=True)
+
+plt.plot(a,b, label='Gráfica')
+
+ 
+plt.xlabel('t')
+plt.ylabel('Velocidad Método de Euler ve(t)')
+plt.legend()
+plt.show()
+
+with open("ye.txt", "w") as archivo: 
+    for i in range(len(ye)): 
+        archivo.write(f"{t[i]}\t,{ye[i]}\t") 
+        archivo.write("\n") 
+
+c, d = np.loadtxt('ye.txt', delimiter=',', unpack=True)
+
+plt.plot(c,d, label='Gráfica')
+
+ 
+plt.xlabel('t')
+plt.ylabel('Posición Método de Euler ye(t)')
+plt.legend()
+plt.show()
